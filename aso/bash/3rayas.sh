94fdf3587b0d7ce64128e901afab73da6d1c0989
Mario Pereira Bermúdez <mpereirabermudez@danielcastelao.org>
2023-10-31 16:38:58 +0100

    Fixes

--- aso/bash/3rayas.sh
+++ aso/bash/3rayas.sh
@@ -1,42 +1,50 @@
 #!/bin/bash
 
-n=0
-while [[ $n -ne 9 ]]
-do
-    read -n1 -rep "Posición: " n_pos # Número de posición
-    while ! [[ $n_pos =~ ^[1-9]$ ]]
-    do
-        read -n1 -rep "Número inválido!. Posición: " n_pos
-    done
-    
-    read -n1 -rep "Símbolo: " simb # Símbolo
-    while ! [[ $simb == "X" && $simb == "O" ]]
-    do
-        read -n1 -rep "Símbolo inválido!. Símbolo: " simb
-    done
-    n=$(($n+1))
-    for (( i=1; i<=9; i++ ))
-    do
-        pos="pos${i}"
-        if [[ $i -eq $n_pos ]]
-        then
-            declare "$pos"="$simb"
-        else
-            pos_e="${!pos}"
-            if [[ $pos_e == "" || $pos_e == " " ]]
-            then
-                declare "$pos"=" "   
-            fi
-        fi
-    clear    
-    done
-    
-echo "|---|---|---|"
-echo "| $pos1 | $pos2 | $pos3 |"
-echo "|---|---|---|"
-echo "| $pos4 | $pos5 | $pos6 |"
-echo "|---|---|---|"
-echo "| $pos7 | $pos8 | $pos9 |"
-echo "|---|---|---|"
+function board() {
+    echo -e "\t${white}|---|---|---|"
+    echo -e "\t| ${reset}${purple}$pos1${reset} | ${purple}$pos2${reset} | ${purple}$pos3${reset}${white} |"
+    echo -e "\t|---|---|---|"
+    echo -e "\t| ${reset}${purple}$pos4${reset} | ${purple}$pos5${reset} | ${purple}$pos6${reset}${white} |"
+    echo -e "\t|---|---|---|"
+    echo -e "\t| ${reset}${purple}$pos7${reset} | ${purple}$pos8${reset} | ${purple}$pos9${reset}${white} |"
+    echo -e "\t|---|---|---|${reset}"
+}
 
-done
+for (( i=1; i<=9; i++ ))
+do
+    pos="pos${i}"
+    declare "$pos"=" " 
+    done
+
+white=$'\001\033[1;97m\002' # Color blanco
+purple=$'\001\033[1;95m\002' # Color morado
+reset=$'\001\033[0m\002' # Reset de colores
+
+board
+
+n=1
+while [[ $n -ne 10 ]]
+do
+    if [[ $n -eq 1 || $n -eq 3 || $n -eq 5 || $n -eq 7 || $n -eq 9 ]]
+    then
+        read -n1 -rep $'\tJugador 1: ' n_pos # Número de posición
+        while [[ $n_pos -lt 1 || $n_pos -gt 9 ]]
+        do
+            read -n1 -rep $'\tNúmero inválido!. Jugador 1: ' n_pos
+        done
+        symb="X"
+    else
+        read -n1 -rep $'\tJugador 2: ' n_pos # Número de posición
+        while [[ $n_pos -lt 1 || $n_pos -gt 9 ]]
+        do
+            read -n1 -rep $'\tNúmero inválido!. Jugador 2: ' n_pos
+        done
+        symb="O"
+    fi
+    pos="pos${n_pos}"
+    declare "$pos"="$symb"
+    n=$(($n+1))
+    clear
+    board
+
+done 
