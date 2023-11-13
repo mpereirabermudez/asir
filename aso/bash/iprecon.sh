30ad2388ea062ea40bc14ef7eef7b5b29bdfe1df
mario <mpereirabermudez@danielcastelao.org>
2023-11-13 02:20:18 +0100

    version 1.0

--- aso/bash/iprecon.sh
+++ aso/bash/iprecon.sh
@@ -3,81 +3,28 @@
 # Script para realizar el reconocimiento de hosts activos en la red
 # Autor: Mario Pereira aka DrDrunk3nst3in
 
-function host() { # Comrpobamos si el host y puerto 22 estan activos
-    if ping -c1 -W1 "$1" > /dev/null 2>&1 
-    then
-        mac_addr=$(arp -an | awk -v ip="($1)" '$2 == ip {print $4}') # Dirección MAC de cada host de la red
-        if nmap -p22 -n -Pn "$1" | grep "22/tcp open" > /dev/null 2>&1 # Comprobamos si el puerto 22 (ssh) está abierto
-        then
-            echo -e "\t${red}[+]${reset} ${green}Host${reset} ${yellow}$1${reset} ${green}at${reset} ${yellow}$mac_addr${reset} ${green}is up (ssh open)${reset}"
-        else
-            echo -e "\t${red}[+]${reset} ${green}Host${reset} ${yellow}$1${reset} ${green}at${reset} ${yellow}$mac_addr${reset} ${green}is up${reset}" 
-        fi
-    fi
-}
-
 # Función para realizar el reconocimiento de hosts activos en la red
 function ip_recon() {
-    read net_addr msk_addr < <(ip a | awk '/inet /{split($2, msk, "/"); split(msk[1], net, "."); if (net[1] != 127 && net[1] != 172) {if (msk[2] == 24) {print net[1]"."net[2]"."net[3]".0" " " msk[2]} else if (msk[2] == 16) {print net[1]"."net[2]".0.0" " " msk[2]} else if (msk[2] == 8) {print net[1]".0.0.0" " " msk[2]}}}') # Dirección y máscara de red
+    read net_addr msk_addr < <(ip a | awk '/inet /{split($2, msk, "/"); split(msk[1], net, "."); if (net[1] != 127 && net[1] != 172) {print net[1]"."net[2]"."net[3]".0" " " msk[2]}}') # Dirección y máscara de red
     echo -e "${red}[+]${reset} ${cyan}Network${reset} ${yellow}${net_addr}/$msk_addr${reset}\n"
-    i=j=k=0
-    if [[ $msk_addr -eq 24 ]]
-    then
-        for (( k=1; k<=$1; k++ ))
-        do
-            {
-            ip_addr=$(echo $net_addr | awk -v host24=$k '{split($1, ip, "."); {print ip[1]"."ip[2]"."ip[3]"."host24}}') # Dirección IP de cada host de la r
-            host $ip_addr
-            } &
-        done
-        wait
-    elif [[ $msk_addr -eq 16 ]]
-    then
-        for (( j=0; j<=$1; j++ ))
-        do
-            for (( k=1; k<=$2; k++ ))
-            do
-                {
-                ip_addr=$(echo $net_addr | awk -v host16=$j -v host24=$k '{split($1, ip, "."); {print ip[1]"."ip[2]"."host16"."host24}}') # Dirección IP de cada host de la red
-                host $ip_addr
-                } &
-            done
-            wait
-        done
-    elif [[ $msk_addr -eq 8 ]]
-    then
-        for (( i=0; i<=$1; i++ ))
-        do
-            for (( j=0; j<=$2; j++ ))
-            do
-                for (( k=1; k<=$3; k++ ))
-                do
-                    {
-                    ip_addr=$(echo $net_addr | awk -v host8=$i -v host16=$j -v host24=$k '{split($1, ip, "."); {print ip[1]"."host8"."host16"."host24}}') # Dirección IP de cada host de la red
-                    host $ip_addr
-                    } &
-                done
-                wait
-            done
-        done
-    fi 
-}
-
-# Función para comprobar si los comandos necesarios están instalados
-function cmd_check() {
-    n_cmd=0 # Contador de comandos no instalados
-    for (( i=1; i<=$#; i++ )) 
+    for (( host=1; host<=$1; host++ ))
     do
-        if ! command -v "${!i}" > /dev/null 2>&1 # Comprobamos si el comando está instalado
+        {
+        IFS='.' read -a ip_parts <<< "$net_addr" # Separamos la dirección IP en 4 partes
+        ip_addr="${ip_parts[0]}.${ip_parts[1]}.${ip_parts[2]}.$host" # Dirección IP de cada host de la red 
+        if ping -c1 -W1 "$ip_addr" > /dev/null 2>&1 
         then
-            echo -e "${red}[!]${reset} ${yellow}${!i}${reset} ${green}is not installed${reset}"
-            n_cmd=$((n_cmd+1)) # Se incrementa el contador de comandos no instalados
+            mac_addr=$(arp -an | awk -v ip="($ip_addr)" '$2 == ip {print $4}') # Dirección MAC de cada host de la red
+            if nmap -p22 --open -T4 --min-rate 5000 -n -Pn "$ip_addr" | grep -q 'open' # Comprobamos si el puerto 22 (ssh) está abierto
+            then
+                echo -e "\t${red}[+]${reset} ${green}Host${reset} ${yellow}$ip_addr${reset} ${green}at${reset} ${yellow}$mac_addr${reset} ${green}is up (ssh open)${reset}"
+            else
+            echo -e "\t${red}[+]${reset} ${green}Host${reset} ${yellow}$ip_addr${reset} ${green}at${reset} ${yellow}$mac_addr${reset} ${green}is up${reset}" 
+            fi
         fi
+        } &
     done
-    if [[ $n_cmd -ne 0 ]] # Si el contador de comandos no instalados es distinto de 0, se aborta el script
-    then
-        exit 1
-    fi
+    wait
 }
 
 # Colores y estilos
@@ -89,41 +36,44 @@ reset=$'\001\033[0m\002' # Reset de colores
 italic=$'\001\033[3m\002' # Itálica
 normal=$'\001\033[m\002' # Normal
 
-if  [[ $1 =~ ^([1-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-4])$ || -z $1 ]] && [[ $2 =~ ^([1-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-4])$ || -z $2 ]] && [[ $3 =~ ^([1-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-4])$ || -z $3 ]] # Comprobamos si el argumento es un número entre 1 y 254
-then
-    echo "                                                                                                                           ";
-    echo "██╗██████╗     ██╗  ██╗ ██████╗ ███████╗████████╗    ██████╗ ██╗███████╗ ██████╗ ██████╗ ██╗   ██╗███████╗██████╗ ██╗   ██╗";
-    echo "██║██╔══██╗    ██║  ██║██╔═══██╗██╔════╝╚══██╔══╝    ██╔══██╗██║██╔════╝██╔════╝██╔═══██╗██║   ██║██╔════╝██╔══██╗╚██╗ ██╔╝";
-    echo "██║██████╔╝    ███████║██║   ██║███████╗   ██║       ██║  ██║██║███████╗██║     ██║   ██║██║   ██║█████╗  ██████╔╝ ╚████╔╝ ";
-    echo "██║██╔═══╝     ██╔══██║██║   ██║╚════██║   ██║       ██║  ██║██║╚════██║██║     ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗  ╚██╔╝  ";
-    echo "██║██║         ██║  ██║╚██████╔╝███████║   ██║       ██████╔╝██║███████║╚██████╗╚██████╔╝ ╚████╔╝ ███████╗██║  ██║   ██║   ";
-    echo "╚═╝╚═╝         ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝       ╚═════╝ ╚═╝╚══════╝ ╚═════╝ ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝   ╚═╝   ";
-    echo "                                                                                                                           ";
-
-    cmd_check ip arp ping nmap # Comprobamos si los comandos necesarios están instalados
-
-    echo -e "${italic}Starting Active Host Discovery...${normal}\n"
-
-    # Comprobamos si se ha pasado un argumento
-    if [[ $# -eq 0 ]]
+num_commands=0 # Contador de comandos no instalados
+commands=("ip" "arp" "ping" "nmap")
+for command in "${commands[@]}"
+do
+    if ! command -v "${command}" > /dev/null 2>&1 # Comprobamos si el comando está instalado
     then
-        ip_recon 254 254 254 # Por defecto se realizará el reconocimiento de todos los hosts de la red
-        echo -e "\n${red}[!]${reset} ${cyan}Scan finished!${reset}\n"
-    elif [[ $# -eq 1 ]]
-    then
-        ip_recon $1 # Se realizará el reconocimiento del número de hosts indicados
-        echo -e "\n${red}[!]${reset} ${cyan}Scan finished!${reset}\n"
-    elif [[ $# -eq 2 ]]
-    then
-        ip_recon $1 $2 # Se realizará el reconocimiento del número de hosts indicados
-        echo -e "\n${red}[!]${reset} ${cyan}Scan finished!${reset}\n"
-    elif [[ $# -eq 3 ]]
-    then
-        ip_recon $1 $2 $3 # Se realizará el reconocimiento del número de hosts indicados
-        echo -e "\n${red}[!]${reset} ${cyan}Scan finished!${reset}\n"
+        echo -e "${red}[!]${reset} ${yellow}${command}${reset} ${green}is not installed${reset}"
+        num_commands=$((num_commands+1)) # Se incrementa el contador de comandos no instalados
     fi
-elif ! ([[ $1 =~ ^([1-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-4])$ || -z $1 ]] && [[ $2 =~ ^([1-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-4])$ || -z $2 ]] && [[ $3 =~ ^([1-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-4])$ || -z $3 ]])
+done
+if [[ $n_cmd -eq 0 ]] # Si el contador de comandos no instalados es distinto de 0, se aborta el script
 then
-    echo -e "${red}[!]${reset} ${yellow}$1${reset} ${cyan}is not a valid number${reset}"
+    if  [[ $1 =~ ^([1-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-4])$ || $# -eq 0 ]] # Comprobamos si el argumento es un número entre 1 y 254
+    then
+        echo "                                                                                                                           ";
+        echo "██╗██████╗     ██╗  ██╗ ██████╗ ███████╗████████╗    ██████╗ ██╗███████╗ ██████╗ ██████╗ ██╗   ██╗███████╗██████╗ ██╗   ██╗";
+        echo "██║██╔══██╗    ██║  ██║██╔═══██╗██╔════╝╚══██╔══╝    ██╔══██╗██║██╔════╝██╔════╝██╔═══██╗██║   ██║██╔════╝██╔══██╗╚██╗ ██╔╝";
+        echo "██║██████╔╝    ███████║██║   ██║███████╗   ██║       ██║  ██║██║███████╗██║     ██║   ██║██║   ██║█████╗  ██████╔╝ ╚████╔╝ ";
+        echo "██║██╔═══╝     ██╔══██║██║   ██║╚════██║   ██║       ██║  ██║██║╚════██║██║     ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗  ╚██╔╝  ";
+        echo "██║██║         ██║  ██║╚██████╔╝███████║   ██║       ██████╔╝██║███████║╚██████╗╚██████╔╝ ╚████╔╝ ███████╗██║  ██║   ██║   ";
+        echo "╚═╝╚═╝         ╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝       ╚═════╝ ╚═╝╚══════╝ ╚═════╝ ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝   ╚═╝   ";
+        echo "                                                                                                                           ";
+
+        echo -e "${italic}Starting Active Host Discovery...${normal}\n"
+
+        # Comprobamos si se ha pasado un argumento
+        if [[ $# -eq 0 ]]
+        then
+            ip_recon 254 # Por defecto se realizará el reconocimiento de todos los hosts de la red
+            echo -e "\n${red}[!]${reset} ${cyan}Scan finished!${reset}\n"
+        else
+            ip_recon $1 # Se realizará el reconocimiento del número de hosts indicados
+            echo -e "\n${red}[!]${reset} ${cyan}Scan finished!${reset}\n"
+        fi
+    else
+        echo -e "${red}[!]${reset} ${yellow}$1${reset} ${cyan}is not a valid number${reset}"
+        exit 1
+    fi
+else
     exit 1
 fi
